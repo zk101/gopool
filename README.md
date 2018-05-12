abbe46ef98279f704a64c15b6041014fea7d2a74
Fletch <daath101@gmail.com>
2018-05-13 09:55:26 +1000

    add notes to config and update readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gopool
 A generic goroutine pooling library
+
+This library provides low level mechanics for maintaining a dynamic pool of goroutines that process tasks.  It is intentionally simple to allow for higher level functionality to be build into the implementation.  As such, concepts such as context, logging, error handling and metrics are not directly impemented by this library, it simply provides the minimum of support for those concepts to exist.
+
+The examples folder holds simple code implemetations of this library.
--- config.go
+++ config.go
@@ -1,6 +1,13 @@
 package gopool
 
 // Config holds static configuration items
+//
+// DaemonExpiry; The number of seconds a daemon will sit idle before exiting.
+// MonitorLoopTimeout; The number of milliseconds of sleep between monitor goroutine cycles
+// PoolSizeMin; The minimum number of daemons kept running
+// PoolSizeMax; The maximum number of daemons in the pool
+// QueueSize; The maximum number of Tasks allowed
+// QueueTimeout; The number of milliseconds the Queue function will wait to queue a Task before returning a timeout
 type Config struct {
 	DaemonExpiry       int
 	MonitorLoopTimeout int
